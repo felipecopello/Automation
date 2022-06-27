a370efee366b0ef702a14c8cb256e5d738ae2d6c
felipe <felipecopello94@gmail.com>
2022-06-27 13:22:58 +0200

    small change

--- src/main/java/com/qaprosoft/carina/demo/gui/pages/YahooMailLoginPage.java
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/YahooMailLoginPage.java
@@ -3,8 +3,6 @@ package com.qaprosoft.carina.demo.gui.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
-import com.qaprosoft.carina.core.foundation.utils.Configuration;
-import com.qaprosoft.carina.core.foundation.utils.R;
 import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.qaprosoft.carina.core.gui.AbstractPage;
 
@@ -18,7 +16,6 @@ public class YahooMailLoginPage extends AbstractPage {
 
 	public YahooMailLoginPage(WebDriver driver) {
 		super(driver);
-		setPageAbsoluteURL(R.CONFIG.get(Configuration.Parameter.URL.getKey()));
 	}
 
 	public void testNextButtonWithoutUser() {
